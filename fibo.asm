97e50f1db81cdc7da72768db12666fd4bf129104
LOISGALLAUD <loisgallaud@hotmail.com>
2023-11-13 21:55:45 +0100

    fibo

--- fibo.asm
+++ fibo.asm
@@ -12,13 +12,13 @@
 main:
 ;------------------------------------------------------------------------------
 ; - pushed rbx on the stack
-; - set ecx to 90
+; - set cx to 10
 ; - set rax to 0
 ; - set rbx to 1
 ;------------------------------------------------------------------------------
         push    rbx                     ; push on the stack 
 
-        mov     ecx, 10                 ; ecx will countdown to 0
+        mov     cx, 10                  ; cx will countdown to 0
         xor     rax, rax                ; rax will hold the current number
         xor     rbx, rbx                ; rbx will hold the next number
         inc     rbx                     ; rbx is originally 1
@@ -35,21 +35,23 @@ print:
         push    rax                     ; caller-save register
         push    rcx                     ; caller-save register
 
+        ; Stack is already aligned because we pushed three 8 byte (3 x 64 bits)
+        ; registers
         mov     rdi, format             ; set 1st parameter (format)
         mov     rsi, rax                ; set 2nd parameter (current_number)
         xor     rax, rax                ; (ABI calling convention)
-
-        ; Stack is already aligned because we pushed three 8 byte (3 x 64 bits)
-        ; registers
         call    printf                  ; printf(format, current_number)
 
         pop     rcx                     ; restore caller-save register
         pop     rax                     ; restore caller-save register
-
+; Fibonacci algorithm
+; rax = current number
+; rbx = next number
+; rcx | ecx | ... = counter
         mov     rdx, rax                ; save the current number
         mov     rax, rbx                ; next number is now current
         add     rbx, rdx                ; get the new next number
-        dec     ecx                     ; count down
+        dec     cx                     ; count down
         jnz     print                   ; if not done counting, do some more
 
         pop     rbx                     ; restore rbx before returning
